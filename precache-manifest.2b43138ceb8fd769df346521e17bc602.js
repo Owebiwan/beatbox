3738bc867b9189b04242d6076796160e52bfa3ee
Godwin Obamina <38545029+Owebiwan@users.noreply.github.com>
2020-10-26 13:33:21 +0100

    Updates

---
+++ precache-manifest.2b43138ceb8fd769df346521e17bc602.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a343dac63eed4cb9c1a296bd2ac0efc",
+    "url": "/beatbox/index.html"
+  },
+  {
+    "revision": "346fe0e1e3edddd85a87",
+    "url": "/beatbox/static/css/main.b48893ac.chunk.css"
+  },
+  {
+    "revision": "5f8143d9a31aba560fce",
+    "url": "/beatbox/static/js/2.428fc212.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/beatbox/static/js/2.428fc212.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "346fe0e1e3edddd85a87",
+    "url": "/beatbox/static/js/main.6768d920.chunk.js"
+  },
+  {
+    "revision": "5a1caab4f5e9049fd465",
+    "url": "/beatbox/static/js/runtime-main.f0e015e2.js"
+  },
+  {
+    "revision": "c534ed5e9ecba739b995ee0f69fe3ade",
+    "url": "/beatbox/static/media/woody.c534ed5e.png"
+  }
+]);
